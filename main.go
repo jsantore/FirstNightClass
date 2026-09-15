8fb953ad2a9b178475cce14c6fcc3b0bdf67db14
shinjitsu <shinjitsu@auxilioabalto.me>
2026-09-14 20:41:29 -0400

    end of day 1

--- main.go
+++ main.go
@@ -4,28 +4,47 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
+type classInfo struct {
+	Name       string
+	Instructor string
+	numCredits int
+}
+
 func main() {
-	classes := make([]string, 0)
+	classes := make([]classInfo, 0)
+	nextClass := classInfo{}
 	cursor := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter class name: ")
 	name, err := cursor.ReadString('\n')
 	if err != nil {
 		panic(err)
 	}
-	classes = append(classes, name)
-	fmt.Print("Enter another class name: ")
-	name, err = cursor.ReadString('\n')
-	if err != nil {
-		panic(err)
-	}
-	classes = append(classes, name)
-	fmt.Print("Enter another class name: ")
-	name, err = cursor.ReadString('\n')
-	if err != nil {
-		panic(err)
-	}
-	classes = append(classes, name)
+	nextClass.Name = name
+	fmt.Print("Enter instructor name: ")
+	teacher, err := cursor.ReadString('\n')
+	nextClass.Instructor = teacher
+	fmt.Print("Enter number of credits: ")
+	creditsAsStr, err := cursor.ReadString('\n')
+	creditsAsStr = strings.Trim(creditsAsStr, "\n")
+	credits, err := strconv.Atoi(creditsAsStr)
+	fmt.Println(err)
+	nextClass.numCredits = credits
+	classes = append(classes, nextClass)
+	//fmt.Print("Enter another class name: ")
+	//name, err = cursor.ReadString('\n')
+	//if err != nil {
+	//	panic(err)
+	//}
+	//classes = append(classes, name)
+	//fmt.Print("Enter another class name: ")
+	//name, err = cursor.ReadString('\n')
+	//if err != nil {
+	//	panic(err)
+	//}
+	//classes = append(classes, name)
 	fmt.Println(classes)
 }
